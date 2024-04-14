b314378bbacb896b768fe8389df590fb51329954
Daniil Orain <dnlorn@gmail.com>
2024-04-14 15:39:00 +0400

    fix mobile version on the main page

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,7 @@ header {
 
 .logo {
   display: block;
-  margin: 0 auto 2rem;
+  margin: 0 auto;
 }
 
 nav {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <main>
+  <div id="projects">
     <div class="projects_wrapper">
       <h2 class="text-2xl font-bold">Current projects</h2>
       <ul>
@@ -10,7 +10,7 @@
           <a href="https://www.youtube.com/@daniilorain/featured" target="_blank">Daniil Orain</a>
         </li>
         <li>
-          <a href="https://orain.school/?utm_source=daniiloraincom" class="underline decoration-dashed" title="It's a Russian-language online school where I teach people how to do YouTube" target="_blank">Онлайн-школа ютьюбинга Даниила Ораина</a>
+          <a href="https://orain.school/?utm_source=daniiloraincom" class="underline decoration-dashed" title="It's a Russian-language online school where I teach people how to do YouTube" target="_blank">Онлайн-школа ютьюбинга</a>
         </li>
         <RouterLink to="/translator">Authoritarian dictionary</RouterLink>
       </ul>
@@ -24,10 +24,28 @@
         </li>
       </ul>
     </div>
-  </main>
+  </div>
 </template>
 
 <style>
+
+@media screen and (max-width: 1024px) {
+  #projects {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 2rem;
+  }
+  .projects_wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+  }
+}
+
 .projects_wrapper {
   &:nth-of-type(n + 2) {
     margin-top: 2em;
