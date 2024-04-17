101b7843e1bd462f83fc1fd7b141f98cb3c64214
Daniil Orain <dnlorn@gmail.com>
2024-04-18 00:04:16 +0400

    fix error mistakes

--- src/views/Translator/Translator.vue
+++ src/views/Translator/Translator.vue
@@ -21,25 +21,41 @@ const hash = ref(route.hash);
 const dynamicSegment = ref(route.params.dynamicSegment || DEFAULT_TRANSLATOR_LANG);
 
 const isLoadingData = ref(false);
+const isError = ref(false);
 
 function update(lang) {
   isLoadingData.value = true;
-  updateInterface(lang).then(() => updateTranslator(lang)).then(() => {
-    isLoadingData.value = false;
-    nextTick();
-  });
+  updateInterface(lang)
+    .then(() => updateTranslator(lang))
+    .then(() => {
+      isLoadingData.value = false;
+      nextTick();
+    })
+    .catch(() => {
+      isError.value = true;
+      isLoadingData.value = false;
+    });
 }
 
 async function updateInterface(lang) {
   const data = await fetch(`${import.meta.env.VITE_SERVER}translator_interface?lang=${lang}`)
-    .then(response => response.json());
+    .then(response => response.json())
+    .catch(error => new Error(error));
 
+  if (data instanceof Error) {
+    throw new Error();
+  }
   return interfaceText.value = data;
 }
 
 async function updateTranslator(lang) {
   const data = await fetch(`${import.meta.env.VITE_SERVER}authoritarian_dictionary?lang=${lang}`)
-    .then(response => response.json());
+    .then(response => response.json())
+    .catch(error => new Error(error));
+
+  if (data instanceof Error) {
+    throw new Error();
+  }
 
   const metadataVal = data.meta;
   let dataWithoutMeta = {...data};
@@ -91,7 +107,25 @@ watch(dictionary, (newVal) => {
   <div class="d-flex w-1/2">
     <div class="card m-3 translator-body">
       <div class="card-body mt-3">
-        <div v-if="!isLoadingData">
+        <div v-if="isError">
+          <div class="bg-red-50 border border-red-200 text-sm text-red-800 rounded-lg p-4 dark:bg-red-800/10 dark:border-red-900 dark:text-red-500" role="alert">
+            <div class="flex">
+              <div class="flex-shrink-0">
+                <svg class="flex-shrink-0 size-4 mt-0.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                  <circle cx="12" cy="12" r="10"></circle>
+                  <path d="m15 9-6 6"></path>
+                  <path d="m9 9 6 6"></path>
+                </svg>
+              </div>
+              <div class="ms-4">
+                <h3 class="text-sm font-semibold">
+                  Ladies and gentlemen, I've messed up somewhere and the dictionary doesn't seem to be functioning
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div v-else-if="!isLoadingData">
           <h2>
             <span class="text-4xl font-bold">{{ interfaceText.value.headline }}&nbsp;</span>
             <span class="text-xl italic">{{ interfaceText.value.headline_small }}</span>
@@ -128,13 +162,13 @@ watch(dictionary, (newVal) => {
       </div>
     </div>
     <TranslatorMeta
-      v-if="!isLoadingData"
+      v-if="!isLoadingData && !isError"
       :interface-text=interfaceText
       class="mt-6"
       :metadata="metadata"
     />
     <div class="card mt-6">
-      <Disqus v-if="!isLoadingData"/>
+      <Disqus v-if="!isLoadingData && !isError"/>
     </div>
   </div>
 </template>
