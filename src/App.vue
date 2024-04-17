f651f0d64f333d1d3fc7292943612111fe7a20a1
Daniil Orain <dnlorn@gmail.com>
2024-04-17 23:46:06 +0400

    fix mobile version

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,10 @@
 <script setup>
-import {RouterView, useRoute} from 'vue-router'
-import { computed } from 'vue';
-
-const route = useRoute();
-
-const showHeader = computed(() => {
-  const allowedRoutes = ['/', '/contact'];
-  return allowedRoutes.includes(route.path);
-});
+import {RouterView} from 'vue-router'
 </script>
 
 <template>
-  <header v-if="showHeader">
-    <img alt="Daniil Orain's profile picture" class="logo" src="@/assets/daniilorain.jpeg" width="320" />
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/contact">Contacts</RouterLink>
-    </nav>
-  </header>
   <RouterView />
 </template>
 
 <style scoped>
-header {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-    margin-top: 1rem;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,32 +2,112 @@
 </script>
 
 <template>
-  <div id="projects">
-    <div class="projects_wrapper">
-      <h2 class="text-2xl font-bold">Current projects</h2>
-      <ul>
-        <li>
-          <a href="https://www.youtube.com/@daniilorain/featured" target="_blank">Daniil Orain</a>
-        </li>
-        <li>
-          <a href="https://orain.school/?utm_source=daniiloraincom" class="underline decoration-dashed" title="It's a Russian-language online school where I teach people how to do YouTube" target="_blank">Онлайн-школа ютьюбинга</a>
-        </li>
-        <RouterLink to="/en/translator">Authoritarian dictionary</RouterLink>
-      </ul>
-    </div>
+  <div id="home-view">
+    <header>
+      <img alt="Daniil Orain's profile picture" class="logo" src="@/assets/daniilorain.jpeg" width="320" />
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/contact">Contacts</RouterLink>
+      </nav>
+    </header>
+    <div id="projects">
+      <div class="projects_wrapper">
+        <h2 class="text-2xl font-bold">Current projects</h2>
+        <ul>
+          <li>
+            <a href="https://www.youtube.com/@daniilorain/featured" target="_blank">Daniil Orain</a>
+          </li>
+          <li>
+            <a href="https://orain.school/?utm_source=daniiloraincom" class="underline decoration-dashed" title="It's a Russian-language online school where I teach people how to do YouTube" target="_blank">Онлайн-школа ютьюбинга</a>
+          </li>
+          <RouterLink to="/en/translator">Authoritarian dictionary</RouterLink>
+        </ul>
+      </div>
 
-    <div class="projects_wrapper">
-      <h2 class="text-2xl font-bold">Previous projects</h2>
-      <ul>
-        <li>
-          <a href="https://www.youtube.com/@1420channel" target="_blank">1420 by Daniil Orain</a>
-        </li>
-      </ul>
+      <div class="projects_wrapper">
+        <h2 class="text-2xl font-bold">Previous projects</h2>
+        <ul>
+          <li>
+            <a href="https://www.youtube.com/@1420channel" target="_blank">1420 by Daniil Orain</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <style>
+#home-view {
+  display: grid;
+  grid-template-columns: 1fr;
+}
+header {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  #home-view {
+    grid-template-columns: 1fr 1fr;
+  }
+  header {
+    margin-left: auto;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+    margin-top: 1rem;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
 
 @media screen and (max-width: 1024px) {
   #projects {
